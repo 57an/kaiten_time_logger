dc2b45816a4d8efd902b61ccc2650f660abb0cc1
stolyarov_as <stolyarov_as@rtsoft.msk.ru>
2025-08-02 07:35:51 +0300

    Форматирование окна

--- src/ui/settings_window.py
+++ src/ui/settings_window.py
@@ -62,25 +62,14 @@ class SettingsWindow(tk.Toplevel):
         )
         repo_entry.pack(padx=5, pady=5)
 
-        time_label = ttk.Label(self, text='Время уведомления (HH:MM):', style='Settings.TLabel')
-        time_label.pack(padx=5, pady=5)
-        self.time_var = tk.StringVar(value=config.notification_time)
-        time_entry = ttk.Entry(
-            self,
-            textvariable=self.time_var,
-            width=10,
-            style='Settings.TEntry',
-        )
-        time_entry.pack(padx=5, pady=5)
-
-        # Add combobox for user roles
-        role_label = ttk.Label(self, text='Выберите роль пользователя:', style='Settings.TLabel')
+        role_label = ttk.Label(self, text='Роль пользователя:', style='Settings.TLabel')
         role_label.pack(padx=5, pady=5)
         self.role_var = tk.StringVar()
         role_combobox = ttk.Combobox(
             self,
             textvariable=self.role_var,
             values=sorted(role for role in user_roles.values()),
+            width=39,  # не 40 из-за кнопки раскрытия списка
         )
         role_combobox.pack(padx=5, pady=5)
         self.role_var.set(user_roles[config.role_id])
@@ -91,10 +80,21 @@ class SettingsWindow(tk.Toplevel):
         author_entry = ttk.Entry(
             self,
             textvariable=self.author,
-            width=15,
+            width=40,
             style='Settings.TEntry',
         )
         author_entry.pack(padx=5, pady=5)
+
+        time_label = ttk.Label(self, text='Время уведомления (HH:MM):', style='Settings.TLabel')
+        time_label.pack(padx=5, pady=5)
+        self.time_var = tk.StringVar(value=config.notification_time)
+        time_entry = ttk.Entry(
+            self,
+            textvariable=self.time_var,
+            width=4,
+            style='Settings.TEntry',
+        )
+        time_entry.pack(padx=5, pady=5)
 
         save_button = ttk.Button(
             self,
@@ -102,7 +102,7 @@ class SettingsWindow(tk.Toplevel):
             command=self.save_settings,
             style='Settings.TButton',
         )
-        save_button.pack(padx=5, pady=10)
+        save_button.pack(padx=5, pady=20)
 
     def save_settings(self):
         Config.save_config(
